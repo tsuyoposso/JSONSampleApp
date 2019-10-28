0974ee6cc75504ca1ab953a059fbf17f65c672eb
tsuyoposso <51411162+tsuyoposso@users.noreply.github.com>
2019-10-28 15:37:04 +0900

    first commit

---
+++ JSONSample/JSONSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONSample
+//
+//  Created by 長坂豪士 on 2019/10/28.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
